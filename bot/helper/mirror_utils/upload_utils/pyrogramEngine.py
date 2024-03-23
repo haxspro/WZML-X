eadac2286754eb9608667402eeb3fd3297f4c14b
noobzhax <azorax@pm.me>
2024-03-23 10:18:20 +0700

    chore: rm

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -183,7 +183,7 @@ class TgUploader:
         msg_user = self.__listener.message.from_user
         if config_dict['LEECH_LOG_ID'] and not self.__listener.excep_chat:
             try:
-                self.__leechmsg = await sendMultiMessage(config_dict['LEECH_LOG_ID'], BotTheme('L_LOG_START', mention=msg_user.mention(style='HTML'), uid=msg_user.id, msg_link=self.__listener.source_url))
+                self.__leechmsg = await sendMultiMessage(config_dict['LEECH_LOG_ID'], BotTheme('L_LOG_START', uid=msg_user.id, msg_link=self.__listener.source_url))
             except Exception as er:
                 await self.__listener.onUploadError(str(er))
                 return False
